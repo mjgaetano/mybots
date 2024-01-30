3c01c01fa7fa029683bd89ebe37b44fd1226e5bb
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2024-01-29 23:51:13 -0500

    One tower

--- generate.py
+++ generate.py
@@ -10,7 +10,8 @@ x = 0
 y = 0
 z = 0.5*height
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
-pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
+for i in range(0, 10):
+    pyrosim.Send_Cube(name="Box", pos=[x,y,z+i*height] , size=[length*(0.9**i),width*(0.9**i),height*(0.9**i)])
+#pyrosim.Send_Cube(name="Box2", pos=[x+length,y,z+height] , size=[length,width,height])
 
 pyrosim.End()
