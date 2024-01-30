adb1293e1b85400f20b94e8f64fa797f8891f100
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2024-01-29 23:34:15 -0500

    Added variables for link position and created two boxes (links)

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
 import pyrosim.pyrosim as pyrosim 
 
-pyrosim.Start_SDF("box.sdf")
+pyrosim.Start_SDF("boxes.sdf")
 
 length = 1
 width = 1
-height = 2
+height = 1
 
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length,width,height])
+x = 0
+y = 0
+z = 0.5*height
+
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+pyrosim.Send_Cube(name="Box2", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()
