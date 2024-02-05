bfb83cc039f783fb763312a8d886c1c4d9437eec
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2024-02-05 18:00:50 -0500

    Defining a robot; urdf files

--- generate.py
+++ generate.py
@@ -1,15 +1,18 @@
 import pyrosim.pyrosim as pyrosim 
 
-pyrosim.Start_SDF("world.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
 
-length = 1
-width = 1
-height = 1
+    pyrosim.Send_Cube(name="Box", pos=[-3,3,0.5] , size=[1,1,1])
 
-x = 0
-y = 0
-z = 0.5*height
+    pyrosim.End()
 
-pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    
+    pyrosim.Send_Cube(name="Torso", pos=[0,0,0.5] , size=[1,1,1])
 
-pyrosim.End()
+    pyrosim.End()
+
+Create_World()
+Create_Robot()
