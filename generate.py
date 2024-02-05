dd3b0f00a460779b9855f4a5c1edee345e82cf1a
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2024-02-05 17:45:45 -0500

    A minimal world

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
 import pyrosim.pyrosim as pyrosim 
 
-pyrosim.Start_SDF("boxes.sdf")
+pyrosim.Start_SDF("world.sdf")
 
 length = 1
 width = 1
@@ -10,9 +10,6 @@ x = 0
 y = 0
 z = 0.5*height
 
-for i in range(-2, 3):
-  for j in range(-2, 3):
-    for k in range(0, 10):
-      pyrosim.Send_Cube(name="Box", pos=[x+(i*length),y+(j*width),z+k*height] , size=[length*(0.9**k),width*(0.9**k),height*(0.9**k)])
+pyrosim.Send_Cube(name="Box", pos=[x,y,z] , size=[length,width,height])
 
 pyrosim.End()
