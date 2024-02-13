384e6eba0382ed301a5f1463784235c45063e108
mjgaetano <102186693+mjgaetano@users.noreply.github.com>
2024-02-13 00:47:37 -0500

    Multiple sensors and prettifying the visualization

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+
+matplotlib.pyplot.plot(backLegSensorValues, label="Back Leg", linewidth=4)
+matplotlib.pyplot.plot(frontLegSensorValues, label="Front Leg")
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -11,7 +11,6 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 p.setGravity(0,0,-98)
 planeId = p.loadURDF("plane.urdf")
-
 robotId = p.loadURDF("body.urdf")
 
 p.loadSDF("world.sdf")
@@ -19,13 +18,15 @@ p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
 
 backLegSensorValues = numpy.zeros(ITERATIONS)
+frontLegSensorValues = numpy.zeros(ITERATIONS)
 
 for i in range(0, ITERATIONS):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
 
 p.disconnect()
 
-numpy.save("data/backLegSensorData.npy", backLegSensorValues)
-print(backLegSensorValues)
+numpy.save("data/backLegSensorValues.npy", backLegSensorValues)
+numpy.save("data/frontLegSensorValues.npy", frontLegSensorValues)
